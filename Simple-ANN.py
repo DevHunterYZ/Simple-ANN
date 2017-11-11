93de59f254833229728b2e687882b29691a5e715
Berkay <32465888+DevHunterYZ@users.noreply.github.com>
2017-11-11 22:00:35 +0300

    Update Simple-ANN.py

--- Simple-ANN.py
+++ Simple-ANN.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import fetch_mldata
 from sklearn.neural_network import MLPClassifier
-# mlp = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=500, alpha=1e-4,
-#                     solver='sgd', verbose=10, tol=1e-4, random_state=1)
+#random_state: rastgele sayı üreteci, solver=çözüm yöntemi, learning_rate_init: ilk öğrenme oranı ,max_iter= maksimim iterasyon(yineleme)
+#hidden_layer_sizes: gizli katmandaki nöron sayıları
 mlp = MLPClassifier(hidden_layer_sizes=(50, 4), max_iter=500, alpha=1e-4,
                     solver='sgd', verbose=10, tol=1e-4, random_state=1,
                     learning_rate_init=.1)
