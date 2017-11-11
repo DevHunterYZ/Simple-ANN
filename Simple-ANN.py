a3b64f06de4429c55dc4eecb815a67466938c48d
Berkay <32465888+DevHunterYZ@users.noreply.github.com>
2017-11-11 21:19:03 +0300

    Create Simple-ANN.py

---
+++ Simple-ANN.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_mldata
+from sklearn.neural_network import MLPClassifier
+# mlp = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=500, alpha=1e-4,
+#                     solver='sgd', verbose=10, tol=1e-4, random_state=1)
+mlp = MLPClassifier(hidden_layer_sizes=(50, 4), max_iter=500, alpha=1e-4,
+                    solver='sgd', verbose=10, tol=1e-4, random_state=1,
+                    learning_rate_init=.1)
+mlp.fit(learnset, learnlabels)
+print("Training set score: %f" % mlp.score(learnset, learnlabels))
+print("Test set score: %f" % mlp.score(learnset, learnlabels))
+mlp.classes_
